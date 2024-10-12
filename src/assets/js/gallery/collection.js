73936c7e9d705818968547455cb9d4a0cb2293d5
Nehemia Telusma <telusma.nehemia@gmail.com>
2024-10-12 19:08:19 +0200

    feat: fetch collection detail api

--- src/assets/js/gallery/collection.js
+++ src/assets/js/gallery/collection.js
@@ -1,4 +1,5 @@
 const collection = {
+  artObjects: [],
 
   init(){
     this.getData();
@@ -8,7 +9,7 @@ const collection = {
     const baseUrl = 'https://www.rijksmuseum.nl/api',
           apiKey = import.meta.env.VITE_API_KEY,
           culture = "en",
-          ps = 10,
+          ps = 5,
           url = `${baseUrl}/${culture}/collection?key=${apiKey}&ps=${ps}`;
 
     fetch(url)
@@ -22,15 +23,54 @@ const collection = {
       if (!data || !data.artObjects) {
         throw new Error("Invalid data");
       }
-      console.log(data);
+
+      this.artObjects.push(...data.artObjects);
+
+      if (this.artObjects.length > 0) {
+        this.artObjects.forEach(art => {
+          console.log(art);
+
+          this.getDetailsData(art.id)
+        });
+      }
+
     })
     .catch(error => {
-      console.error('There was a problem with the fetch operation:', error);
-      const flashContainer = document.querySelector('.flash');
-      const flashContent = flashContainer.querySelector('span');
-      flashContainer.classList.add('error');
-      flashContent.textContent = `An error occurred during data fetching : ${error.message}`;
+      this.handleFetchingError(error)
     });
+  },
+
+  getDetailsData(artId) {
+    const baseUrl = 'https://www.rijksmuseum.nl/api',
+          apiKey = import.meta.env.VITE_API_KEY,
+          culture = "en",
+          url = `${baseUrl}/${culture}/collection/${artId}?key=${apiKey}`;
+
+    fetch(url)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(detailData => {
+        this.showDetails(detailData.artObject);
+      })
+      .catch(error => {
+        this.handleFetchingError(error)
+      });
+  },
+
+  handleFetchingError(error){
+    console.error('There was a problem with the fetch operation:', error);
+    const flashContainer = document.querySelector('.flash');
+    const flashContent = flashContainer.querySelector('span');
+    flashContainer.classList.add('error');
+    flashContent.textContent = `An error occurred during data fetching : ${error.message}`;
+  },
+
+  showDetails(data){
+    console.log(data)
   }
 }
 
